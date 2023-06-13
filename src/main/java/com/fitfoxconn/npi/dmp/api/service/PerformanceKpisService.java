ee45269b3393127270f71efb25d5987d15ed9b60
FIT1037 <ken.sp.chiu@fit-foxconn.com>
2023-06-13 15:26:44 +0800

    DM-109 開發「依設備、日期、課別(特定時分)取得生產指標」的API

--- src/main/java/com/fitfoxconn/npi/dmp/api/service/PerformanceKpisService.java
+++ src/main/java/com/fitfoxconn/npi/dmp/api/service/PerformanceKpisService.java
@@ -1,8 +1,10 @@
 package com.fitfoxconn.npi.dmp.api.service;
 
 import com.fitfoxconn.npi.dmp.api.common.constant.ShiftEnum;
+import com.fitfoxconn.npi.dmp.api.entity.ods.LogTjOeeEntity;
 import com.fitfoxconn.npi.dmp.api.entity.ods.PzLineEntity;
 import com.fitfoxconn.npi.dmp.api.entity.ods.TjOeeEntity;
+import com.fitfoxconn.npi.dmp.api.repository.ods.LogTjOeeDao;
 import com.fitfoxconn.npi.dmp.api.repository.ods.PzLineDao;
 import com.fitfoxconn.npi.dmp.api.repository.ods.TjOeeDao;
 import com.fitfoxconn.npi.dmp.api.service.PerformanceKpisService.GetMultiDatedPerformanceKpisOutput.Kpis;
@@ -11,6 +13,8 @@ import com.fitfoxconn.npi.dmp.api.service.common.OrganizationUtilService;
 import com.fitfoxconn.npi.dmp.api.service.common.OrganizationUtilService.EquipmentInfo;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -37,6 +41,9 @@ public class PerformanceKpisService {
   private OrganizationUtilService organizationUtilService;
   @Autowired
   private PzLineDao pzLineDao;
+
+  @Autowired
+  private LogTjOeeDao logTjOeeDao;
 
 
   /**
@@ -83,7 +90,8 @@ public class PerformanceKpisService {
   /**
    * 抓取多個設備，某天的效率相關KPI
    */
-  public GetSingleDatedPerformanceKpisOutput getSingleDatedPerformanceKpis(List<Integer> equipmentIds,
+  public GetSingleDatedPerformanceKpisOutput getSingleDatedPerformanceKpis(
+      List<Integer> equipmentIds,
       LocalDate queryDate,
       ShiftEnum shiftEnum) {
 
@@ -96,16 +104,12 @@ public class PerformanceKpisService {
         equipmentIds);
 
     //取得良率目標 from pz_Line
-    Map<String, String> distinctLineCode = new HashMap<>();
-    equipmentInfoMap.values().forEach(v -> {
-      if (!distinctLineCode.containsKey(v.getLineOrganizationCode())) {
-        distinctLineCode.put(v.getLineOrganizationCode(), v.getLineOrganizationCode());
-      }
-    });
-    List<PzLineEntity> pzLineEntityList = this.pzLineDao.findByLineOrganizationCodeIn(
-        distinctLineCode.values().stream().toList());
-    Map<String, PzLineEntity> lineMap = pzLineEntityList.stream()
-        .collect(Collectors.toMap(PzLineEntity::getLineOrganizationCode, Function.identity()));
+    List<String> LineOrganizationCodeList = equipmentInfoMap.values().stream()
+        .map(EquipmentInfo::getLineOrganizationCode)
+        .distinct()
+        .collect(
+            Collectors.toList());
+    Map<String, PzLineEntity> lineMap = this.getPzLineEntityMap(LineOrganizationCodeList);
 
     //組成output
     List<GetSingleDatedPerformanceKpisOutput.Kpis> kpis = new ArrayList<>();
@@ -128,6 +132,75 @@ public class PerformanceKpisService {
         .build();
 
     return result;
+  }
+
+  /**
+   * 抓取某一天，依課別區分，多個設備的效率相關KPI
+   */
+  public List<GetSingleDatedClassedPerformanceKpisOutput> getSingleDatedClassedPerformanceKpis(
+      List<Integer> equipmentIds,
+      LocalDate queryDate,
+      List<LocalTime> boundaryTimes) {
+
+    //取得效率相關KPI資訊
+    Map<LocalTime, List<LogTjOeeEntity>> logTjOeeEntitiesMap = new HashMap<>();
+    boundaryTimes.forEach(t -> {
+      LocalDateTime boundaryDateTime = queryDate.atTime(t);
+      List<LogTjOeeEntity> logTjOeeEntities = this.logTjOeeDao.getLatestByShiftDateAndTimeAndEquipmentIdIn(
+          queryDate, boundaryDateTime, equipmentIds);
+      logTjOeeEntitiesMap.put(t, logTjOeeEntities);
+    });
+
+    //取得設備資訊，如設備名稱，functionName
+    Map<Integer, EquipmentInfo> equipmentInfoMap = this.organizationUtilService.getFunctionNameMapByEquipmentIds(
+        equipmentIds);
+
+    //取得良率目標 from pz_Line
+    List<String> LineOrganizationCodeList = equipmentInfoMap.values().stream()
+        .map(EquipmentInfo::getLineOrganizationCode)
+        .distinct()
+        .collect(
+            Collectors.toList());
+    Map<String, PzLineEntity> lineMap = this.getPzLineEntityMap(LineOrganizationCodeList);
+
+    //組成output
+    List<GetSingleDatedClassedPerformanceKpisOutput> result = new ArrayList<>();
+
+    logTjOeeEntitiesMap.forEach((k, v) -> {
+      List<GetSingleDatedClassedPerformanceKpisOutput.Kpis> kpis = new ArrayList<>();
+      v.forEach(i -> {
+        GetSingleDatedClassedPerformanceKpisOutput.Kpis kpi = new GetSingleDatedClassedPerformanceKpisOutput.Kpis();
+        BeanUtils.copyProperties(i, kpi);
+        kpi.setEquipmentName(equipmentInfoMap.get(i.getEquipmentId()).getEquipmentName());
+        kpi.setLineName(equipmentInfoMap.get(i.getEquipmentId()).getLineName());
+        kpi.setFunctionName(equipmentInfoMap.get(i.getEquipmentId()).getFunctionName());
+        kpi.setAchievementRate(KpiUtil.getAchievementRate(i));
+        kpi.setQualityTarget(
+            lineMap.get(equipmentInfoMap.get(i.getEquipmentId()).getLineOrganizationCode())
+                .getQualityTarget());
+        kpis.add(kpi);
+      });
+
+      GetSingleDatedClassedPerformanceKpisOutput output = GetSingleDatedClassedPerformanceKpisOutput.builder()
+          .date(queryDate)
+          .time(k)
+          .kpis(kpis)
+          .build();
+      result.add(output);
+    });
+
+    return result;
+  }
+
+  /**
+   * 取得以LineOrganizationCode為Key, PzLineEntity為value的Map
+   */
+  private Map<String, PzLineEntity> getPzLineEntityMap(List<String> lineOrganizationCode) {
+    List<PzLineEntity> pzLineEntityList = this.pzLineDao.findByLineOrganizationCodeIn(
+        lineOrganizationCode);
+    Map<String, PzLineEntity> lineMap = pzLineEntityList.stream()
+        .collect(Collectors.toMap(PzLineEntity::getLineOrganizationCode, Function.identity()));
+    return lineMap;
   }
 
   @Builder
@@ -245,6 +318,113 @@ public class PerformanceKpisService {
      * 日期
      */
     private LocalDate date;
+    /**
+     * KPI資料
+     */
+    private List<Kpis> kpis;
+
+    @lombok.Data
+    public static class Kpis {
+
+      /**
+       * 設備編號
+       */
+      private int equipmentId;
+      /**
+       * 設備名稱
+       */
+      private String equipmentName;
+      /**
+       * 所屬line名稱
+       */
+      private String lineName;
+      /**
+       * 所屬function名稱
+       */
+      private String functionName;
+      /**
+       * 計畫量(pcs)
+       */
+      private Integer plannedProduction;
+      /**
+       * 產出目標(pcs)
+       */
+      private Integer productionTarget;
+      /**
+       * 實際產出(pcs)
+       */
+      private Integer totalCount;
+      /**
+       * 良品數(pcs)
+       */
+      private Integer goodCount;
+      /**
+       * 良率目標(%)
+       */
+      private Double qualityTarget;
+      /**
+       * 良率實際(%)
+       */
+      private BigDecimal quality;
+      /**
+       * 達成率(%)
+       */
+      private BigDecimal achievementRate;
+      /**
+       * 總投入時間(秒)
+       */
+      private Double totalTime;
+      /**
+       * 宕機時間(秒)
+       */
+      private Double unplannedStopTime;
+      /**
+       * 計畫停機時間(秒)
+       */
+      private Double plannedStopTime;
+      /**
+       * 負荷時間(秒)
+       */
+      private Double plannedProductionTime;
+      /**
+       * 稼動時間(秒)
+       */
+      private Double runTime;
+      /**
+       * 時間稼動率(%)
+       */
+      private BigDecimal availability;
+      /**
+       * 理論週期
+       */
+      private Double idealCycleTime;
+      /**
+       * 性能稼動率(%)
+       */
+      private BigDecimal performance;
+      /**
+       * OEE1
+       */
+      private BigDecimal oee1;
+      /**
+       * OEE
+       */
+      private BigDecimal oee;
+    }
+  }
+
+  @Builder
+  @NoArgsConstructor
+  @AllArgsConstructor
+  @Data
+  public static class GetSingleDatedClassedPerformanceKpisOutput {
+
+    /**
+     * 日期
+     */
+    private LocalDate date;
+
+    private LocalTime time;
     /**
      * KPI資料
      */
